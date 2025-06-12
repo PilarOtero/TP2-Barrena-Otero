cbd78c5336e077fac413b024250ebdc7d91c8216
Pilar Otero <potero@udesa.edu.ar>
2025-06-12 18:37:38 -0300

    organice como dice en el programa del curso y arregle el informe

---
+++ Trabajo Practico/punto 1/build/CMakeFiles/project1.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/pilarotero/Desktop/2 año/paradigmas de programacion/TP2_Barrena_Otero/Trabajo Practico/punto 1/example/main.cpp" "CMakeFiles/project1.dir/example/main.cpp.o" "gcc" "CMakeFiles/project1.dir/example/main.cpp.o.d"
+  "/home/pilarotero/Desktop/2 año/paradigmas de programacion/TP2_Barrena_Otero/Trabajo Practico/punto 1/src/pokedex.cpp" "CMakeFiles/project1.dir/src/pokedex.cpp.o" "gcc" "CMakeFiles/project1.dir/src/pokedex.cpp.o.d"
+  "/home/pilarotero/Desktop/2 año/paradigmas de programacion/TP2_Barrena_Otero/Trabajo Practico/punto 1/src/pokemon.cpp" "CMakeFiles/project1.dir/src/pokemon.cpp.o" "gcc" "CMakeFiles/project1.dir/src/pokemon.cpp.o.d"
+  "/home/pilarotero/Desktop/2 año/paradigmas de programacion/TP2_Barrena_Otero/Trabajo Practico/punto 1/src/pokemonInfo.cpp" "CMakeFiles/project1.dir/src/pokemonInfo.cpp.o" "gcc" "CMakeFiles/project1.dir/src/pokemonInfo.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Trabajo Practico/punto 2/build/CMakeFiles/project2.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/pilarotero/Desktop/2 año/paradigmas de programacion/TP2_Barrena_Otero/Trabajo Practico/punto 2/example/main.cpp" "CMakeFiles/project2.dir/example/main.cpp.o" "gcc" "CMakeFiles/project2.dir/example/main.cpp.o.d"
+  "/home/pilarotero/Desktop/2 año/paradigmas de programacion/TP2_Barrena_Otero/Trabajo Practico/punto 2/src/simulador.cpp" "CMakeFiles/project2.dir/src/simulador.cpp.o" "gcc" "CMakeFiles/project2.dir/src/simulador.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
